47df4734edf0251e6570bb994ae420fd53abcd9e
Andrew Hayhurst <hayhurst.andrew@gmail.com>
2021-02-16 21:54:31 -0800

    Add alternatingSplit solution

---
+++ alternatingSplit.js
@@ -0,0 +1,46 @@
+// For building the encrypted string:
+// Take every 2nd char from the string, then the other chars, that are not every 2nd char, and concat them as new String.
+// Do this n times!
+
+// Examples:
+
+// "This is a test!", 1 -> "hsi  etTi sats!"
+// "This is a test!", 2 -> "hsi  etTi sats!" -> "s eT ashi tist!"
+// Write two methods:
+
+// function encrypt(text, n)
+// function decrypt(encryptedText, n)
+// For both methods:
+// If the input-string is null or empty return exactly this value!
+// If n is <= 0 then return the input text.
+
+// https://www.codewars.com/kata/simple-encryption-number-1-alternating-split
+
+function encrypt(text, n) {
+  if(typeof text !== 'string') return null;
+  let encryptedArray = text.split('');
+  for(let i = 0; i < n; i++) {
+    let evens = [], odds = [];
+    encryptedArray.forEach((char, index) => {
+      (index % 2 === 1) ? odds.push(char) : evens.push(char);
+    })
+    encryptedArray = odds.concat(evens);
+  }
+  return encryptedArray.join('');
+}
+
+function decrypt(encryptedText, n) {
+  if(typeof encryptedText !== 'string') return null;
+  let decryptedArray = encryptedText.split('');
+  const midpoint = decryptedArray.length / 2
+  for(let i = 0; i < n; i++) {
+    let odds = decryptedArray.slice(0, midpoint);
+    let evens = decryptedArray.slice(midpoint);
+    decryptedArray = [];
+    while(odds.length > 0) {
+      decryptedArray.push(evens.shift(), odds.shift());
+    }
+    if(evens.length > 0) decryptedArray.push(evens.shift());
+  }
+  return decryptedArray.join('');
+}
